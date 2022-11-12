508d490688e75ec89ed939f080cc080a080dd632
Megha Jibhakate <106766775+meghajibhakate@users.noreply.github.com>
2022-11-12 13:35:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+function myFunction() {
+    if (document.getElementById("drop").style.display == "none") {
+        document.getElementById("drop").style.display = "flex";
+    } else {
+        document.getElementById("drop").style.display = "none";
+    }
+}
+
+
+function eventsClick(){
+    if (document.getElementById("drop-events").style.display == "none") {
+        document.getElementById("drop-events").style.display = "flex";
+    } else {
+        document.getElementById("drop-events").style.display = "none";
+    } 
+}
+function eventClick(){
+    if (document.getElementById("drop-newevent").style.display == "none") {
+        document.getElementById("drop-newevent").style.display = "flex";
+    } else {
+        document.getElementById("drop-newevent").style.display = "none";
+    } 
+}
+function aboutClick(){
+    if (document.getElementById("drop-about").style.display == "none") {
+        document.getElementById("drop-about").style.display = "flex";
+    } else {
+        document.getElementById("drop-about").style.display = "none";
+    } 
+}
+
+function newaboutClick(){
+    if (document.getElementById("drop-newabout").style.display == "none") {
+        document.getElementById("drop-newabout").style.display = "flex";
+    } else {
+        document.getElementById("drop-newabout").style.display = "none";
+    } 
+}
